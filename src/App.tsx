8fd37233a62acbc32d568734e4df1d2aa4a18641
Rustam-R-D-S <https://github.com/Rustam-R-D-S/home-work-51.git>
2024-05-30 23:22:41 +0600

    add type of ingridient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import "./App.css"
 
+
 const App = () => {
+
+
+
     return (
         <>
             <div className={"container"}>
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface ingridient{
+    name:string;
+    price:number;
+    image:string;
+}
